107e978557d7997b7e473714611d185f5b05303f
Alljuly <ajsc.elsie@gmail.com>
2025-03-26 14:31:58 +0000

    fix uso de pandas em query_item_by_ID

--- SMGPAT/queries/incorporation/query_item_by_ID.py
+++ SMGPAT/queries/incorporation/query_item_by_ID.py
@@ -20,8 +20,9 @@ PLAQUETA_FINAL_ID = 'vPATPLAQUETAFIN'
 BUTTON_QUERY = 'BTNCONSULTAR'
 
 def load_plaquetas(csv_path: str):
-    plaqueta_init_value = read_csv(csv_path, usecols['plaqueta_inicial'])
-    plaqueta_final_value = read_csv(csv_path, usecols['plaqueta_final'])
+    df = pd.read_csv(csv_path, usecols=['plaqueta_inicial', 'plaqueta_final'])
+    plaqueta_init_value = df['plaqueta_inicial'].iloc[0]
+    plaqueta_final_value = df['plaqueta_final'].iloc[0] 
 
     return [plaqueta_init_value, plaqueta_final_value]
 
