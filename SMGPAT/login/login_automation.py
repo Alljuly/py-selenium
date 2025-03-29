2b24d1c39ab310c3f71f2496620a42260aae7a32
Alice <ajsc1@aluno.ifal.edu.br>
2025-03-28 21:49:57 -0300

    refatorando login para patrimonio

--- SMGPAT/login/login_automation.py
+++ SMGPAT/login/login_automation.py
@@ -1,28 +1,16 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-from SMGPAT.tools import get_navigator
-from SMGPAT.utils import LOGIN_FIELDSET_ID,PASSWORD_FIELDSET_ID,BUTTOM_FORM_LOGIN_NAME,BUTTOM_FORM_MODULE_ID
-from SMGPAT.settings import *
-
-
+from SMGPAT.tools import get_navigator, clear_and_send, wait_and_click
+from SMGPAT.utils import LOGIN_FIELDSET_ID,PASSWORD_FIELDSET_ID, BUTTOM_FORM_MODULE_ID, BUTTOM_FORM_LOGIN_NAME
+from SMGPAT.settings import USER_LOGIN, PASSWORD_LOGIN
 
 def login_patrimonio():
     navigator = get_navigator()
 
-    wait = WebDriverWait(navigator, 30)
+    clear_and_send(navigator, By.ID, LOGIN_FIELDSET_ID, USER_LOGIN)
+    clear_and_send(navigator, By.ID, PASSWORD_FIELDSET_ID, PASSWORD_LOGIN)
 
-    login_fieldset = wait.until(EC.element_to_be_clickable((By.ID, LOGIN_FIELDSET_ID)))
-    password_fieldset = wait.until(EC.element_to_be_clickable((By.ID, PASSWORD_FIELDSET_ID)))
-
-    login_fieldset.send_keys(USER_LOGIN)
-    password_fieldset.send_keys(PASSWORD_LOGIN)
-
-    login_form = wait.until(EC.element_to_be_clickable((By.NAME, BUTTOM_FORM_LOGIN_NAME)))
-    login_form.click()
-
-    module_form = wait.until(EC.element_to_be_clickable((By.ID, BUTTOM_FORM_MODULE_ID)))
-    module_form.click()
-
+    wait_and_click(navigator, By.NAME, BUTTOM_FORM_LOGIN_NAME)
+    wait_and_click(navigator, By.ID, BUTTOM_FORM_MODULE_ID)
+    
     return navigator
