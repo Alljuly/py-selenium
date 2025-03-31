f38f0218ee30ecda8a34f0060499b9e62eeff501
Alice <ajsc1@aluno.ifal.edu.br>
2025-03-31 13:20:48 -0300

    removendo dados de empenho do csv

--- SMGPAT/tools/generate_query_csv.py
+++ SMGPAT/tools/generate_query_csv.py
@@ -18,20 +18,12 @@ def get_item_description(navigator, index):
             "material_name": wait_presence_get_text(navigator, By.ID, MATERIAL_NAME_ID),
             "nota_fiscal": wait_presence_get_text(navigator, By.ID, NOTA_FISCAL_ID),
             "serie_nota_fiscal": wait_presence_get_text(navigator, By.ID, SERIE_NOTA_FISCAL_ID),
-            "numero_empenho": '',
-            "ano_empenho": '',
             "incluido_por": wait_presence_get_text(navigator, By.ID, INCLUIDO_POR_ID),
             "incluido_em": wait_presence_get_text(navigator, By.ID, INCLUIDO_EM_ID),
             "modificado_por": wait_presence_get_text(navigator, By.ID, MODIFICADO_POR_ID),
             "ultima_modificacao": wait_presence_get_text(navigator, By.ID, ULTIMA_MODIFICACAO_ID),  
            }
     
-    empenho = wait_visibility(navigator, By.ID, 'W0378GriddetalhesContainerDiv')
-
-    if empenho:
-        item_description["numero_empenho"] = wait_presence_get_text(navigator, By.ID, NUMERO_EMPENHO_ID)
-        item_description["ano_empenho"] = wait_presence_get_text(navigator, By.ID, ANO_EMPENHO_ID)
-
     wait_and_click(navigator, By.NAME, RETURN_FORM_NAME)        
     
     return item_description
@@ -55,30 +47,13 @@ def generate_query_csv(navigator, query_rows = None, items = None):
     else:
         list_items = items
 
-    item_description = {
-            "patplaqueta": '',
-            "status": '',
-            "organograma_name": '',
-            "material_id": '',
-            "material_name": '',
-            "nota_fiscal": '',
-            "serie_nota_fiscal": '',
-            "numero_empenho": '',
-            "ano_empenho": '',
-            "incluido_por": '',
-            "incluido_em": '',
-            "modificado_por": '',
-            "ultima_modificacao": wait_presence_get_text(navigator, By.ID, ULTIMA_MODIFICACAO_ID),  
-
-        }
-    
     if list_items:
-        header = item_description.keys()
+ 
         current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         result_csv = f'resultado_consulta_{current_time}.csv' 
 
         with open(result_csv, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=header)
+            writer = csv.DictWriter(file, fieldnames=HEADERS_QUERY_CSV)
             writer.writeheader() 
 
             for item in list_items:
