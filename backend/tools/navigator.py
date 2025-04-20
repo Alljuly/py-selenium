53cfa178710062affd4798306ac9decacebd41ce
Alice <ajsc1@aluno.ifal.edu.br>
2025-04-20 14:36:21 -0300

    upgrade navigator arguments

--- backend/tools/navigator.py
+++ backend/tools/navigator.py
@@ -9,10 +9,15 @@ CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
 
 def open_navigator():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")  # Mais leve que o antigo headless
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--disable-background-networking")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument("--single-process")
+    options.add_argument("--remote-debugging-port=9222")
     
     options.binary_location = CHROMIUM_PATH
 
@@ -28,10 +33,15 @@ def open_navigator():
 
 def open_navigator_with_cookies(cookies):
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new") 
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--disable-background-networking")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+    options.add_argument("--single-process")
+    options.add_argument("--remote-debugging-port=9222")
 
     options.binary_location = CHROMIUM_PATH
 
